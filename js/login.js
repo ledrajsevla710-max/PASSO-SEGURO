e25b7def862fa954ffc82a28a113ac25eb590d5a
JARDEL ALVES <ledrajsevla710@gmail.com>
2026-07-10 13:33:26 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,19 +1,1 @@
-async function login() {
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
 
-    const resposta = await window.enviar({ acao: "login", email: email, senha: senha });
-
-    if (resposta.sucesso) {
-        // Salva TUDO no localStorage para o Dashboard ler
-        localStorage.setItem("nome", resposta.usuario.nome);
-        localStorage.setItem("email", resposta.usuario.email);
-        localStorage.setItem("telefone", resposta.usuario.telefone);
-        localStorage.setItem("nascimento", resposta.usuario.nascimento);
-        localStorage.setItem("cidade", resposta.usuario.cidade);
-        localStorage.setItem("uf", resposta.usuario.uf);
-        window.location.href = "dashboard.html";
-    } else {
-        alert(resposta.mensagem);
-    }
-}
