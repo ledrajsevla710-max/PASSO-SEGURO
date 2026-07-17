14331975f2c774a5251c999981b21f147810e525
JARDEL ALVES <ledrajsevla710@gmail.com>
2026-07-17 16:42:47 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 
 
 const API_URL =
-"https://script.google.com/macros/s/AKfycbxiqXvtwk2n63j16cw6t6djePX6LNIkqfFFD2eJxGbEZ-YAWh1TgnluSgmtZlVGD-YR/exec";
+"https://script.google.com/macros/s/AKfycbzOoHiXCW1xl0ivYwRezjj8Hqz0V9cZ5T6Tn1j_wg2ZlPSgE7RioXnzuHdVpjcX7NDE/exec";
 
 
 const APP_NOME = "Passo Seguro";
