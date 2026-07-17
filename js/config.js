ee251f1e459877601efb8e20cf9566e61714a35b
JARDEL ALVES <ledrajsevla710@gmail.com>
2026-07-17 14:23:16 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
+// ==========================================
+// PASSO SEGURO
+// CONFIGURAÇÕES GERAIS
+// ==========================================
 
+// URL da API do Google Apps Script
+const API_URL =
+"https://script.google.com/macros/s/AKfycbxiqXvtwk2n63j16cw6t6djePX6LNIkqfFFD2eJxGbEZ-YAWh1TgnluSgmtZlVGD-YR/exec";
+
+// Nome do sistema
+const APP_NOME = "Passo Seguro";
+
+// Versão
+const APP_VERSAO = "1.0.0";
